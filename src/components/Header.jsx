eec1e5219986b90a25678031c1dd410d04d4ba88
ABigJuicyBurger <70484524+ABigJuicyBurger@users.noreply.github.com>
2025-01-02 11:40:54 -0700

    added routing and a link from login -> guest

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header">
       <h1 className="title"> Hermes40</h1>
-      <button>Log-In</button>
+      <Link to="login">Log-In</Link>
       <button>Post Job</button>
     </div>
   );
